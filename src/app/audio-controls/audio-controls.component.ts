4fc61723a969233861fcd19a92ab22e25104367d
Jeremy Ellington <jme@eminor.net>
2025-03-17 17:04:05 -0500

    Fix frequency slider to conform to proper logarithmic scaling

--- src/app/audio-controls/audio-controls.component.ts
+++ src/app/audio-controls/audio-controls.component.ts
@@ -4,13 +4,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input'; 
 import { MatFormFieldModule } from '@angular/material/form-field'; 
-import { MatSliderModule } from '@angular/material/slider'; 
+import { MatSliderModule, MatSliderVisualThumb } from '@angular/material/slider'; 
 import { MatButtonToggleModule } from '@angular/material/button-toggle'; 
+import { SlicePipe } from '@angular/common';
 
-const FREQUENCY_DEFAULT_HZ = 440;  // Concert A
 const FREQUENCY_MIN_HZ = 20;
 const FREQUENCY_MAX_HZ = 20000;
-
 const VOLUME_MAX = 100;
 const VOLUME_DEFAULT = 10;
 
@@ -33,10 +32,8 @@ export class AudioControlsComponent implements OnInit, OnDestroy {
   audioContext: AudioContext;
   oscillator: OscillatorNode;
   gainNode: GainNode;
-
-  minFreq = 20;    // Minimum audible frequency ~20Hz
-  maxFreq = 20000; // Maximum audible frequency ~20kHz
-  displayFrequency = this.minFreq;
+  sliderValue = 0.5;
+  frequency = this.sliderValueToHz(this.sliderValue);
 
   /**
    * Creates a form group for the audio controls.
@@ -47,18 +44,12 @@ export class AudioControlsComponent implements OnInit, OnDestroy {
    * @param formBuilder 
    */
   constructor(private formBuilder: FormBuilder) {
-    // Calculate the initial slider position for 440Hz using the inverse of
-    // logarithmic mapping log(frequency/minFreq) / log(maxFreq/minFreq) =
-    // normalized value
-    const normalizedValue =
-      Math.log(FREQUENCY_DEFAULT_HZ / this.minFreq) / Math.log(this.maxFreq / this.minFreq);
-    
-    const initialSliderValue = normalizedValue * 20000; // Scaling back to slider range
+    const initialHz = this.sliderValueToHz(0.5);
 
     this.audioControlsFormGroup = this.formBuilder.group({
-      hzSlider: [440],             // Starting slider value for UI
-      hz: [FREQUENCY_DEFAULT_HZ],  // Actual frequency used for audio
-      volume: [VOLUME_DEFAULT],    // Safe
+      hzSlider: [0.5],          // Starting slider value for UI
+      hz: [initialHz],          // Actual frequency used for audio
+      volume: [VOLUME_DEFAULT], // Safe
       startStop: [false],
     });
 
@@ -66,12 +57,10 @@ export class AudioControlsComponent implements OnInit, OnDestroy {
     this.audioContext = new (window.AudioContext || (window as any).webkitAudioContext)();
     this.oscillator = this.audioContext.createOscillator();
     this.gainNode = this.audioContext.createGain();
-
     this.gainNode.gain.value = VOLUME_DEFAULT / 100;
+    this.oscillator.frequency.value = initialHz;
 
-    this.oscillator.frequency.value = FREQUENCY_DEFAULT_HZ;
-
-    // Connect oscillator --> gainNode --> destination
+    // Wire things up: oscillator --> gainNode --> destination
     this.oscillator.connect(this.gainNode);
     this.gainNode.connect(this.audioContext.destination);
   }
@@ -79,11 +68,6 @@ export class AudioControlsComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     // Start the oscillator
     this.oscillator.start();
-
-    // Subscribe to changes in the hz form control
-    this.audioControlsFormGroup.get('hz')!.valueChanges.subscribe((newHz: number) => {
-      this.oscillator.frequency.setValueAtTime(newHz, this.audioContext.currentTime);
-    });
 
     // Subscribe to changes in the volume form control
     this.audioControlsFormGroup.get('volume')!.valueChanges.subscribe((newVolume: number) => {
@@ -99,24 +83,31 @@ export class AudioControlsComponent implements OnInit, OnDestroy {
       }
     });
 
-    // Subscribe to silder changes
-    this.audioControlsFormGroup.get('hzSlider')!.valueChanges.subscribe(this.handleFrequencySliderChange.bind(this));
+    // Subscribe to frequency slider changes
+    this.audioControlsFormGroup.get('hzSlider')!.valueChanges.subscribe(
+      this.handleFrequencySliderChange.bind(this));
+  }
+
+  // Returns a frequency appropriate for a slider, where the slider value is
+  // between 0 and 1.
+  private sliderValueToHz(sliderValue: number): number {
+    return Math.round(
+      FREQUENCY_MIN_HZ *
+      Math.pow(
+      Math.E,
+      Math.log(FREQUENCY_MAX_HZ / FREQUENCY_MIN_HZ) * sliderValue));
   }
 
   // Handler for frequency slider changes
   private handleFrequencySliderChange(sliderValue: number): void {
-    // Convert slider value (0-100) to a frequency using logarithmic scale
-    const normalizedValue = sliderValue / 20000;
-    const frequency = this.minFreq * Math.pow(this.maxFreq / this.minFreq, normalizedValue);
-
-    // Round for display
-    this.displayFrequency = Math.round(frequency);
+    this.sliderValue = sliderValue;
+    this.frequency = this.sliderValueToHz(this.sliderValue);
 
     // Update the actual frequency value in the form for audio processing
-    this.audioControlsFormGroup.get('hz')!.setValue(frequency, {emitEvent: false});
+    this.audioControlsFormGroup.get('hz')!.setValue(this.frequency, {emitEvent: false});
 
     // Update the audio node if it's playing
-    this.updateFrequency(frequency);
+    this.updateFrequency(this.frequency);
   }
 
   // Handles the button toggle for start/stop.
